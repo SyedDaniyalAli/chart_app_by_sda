1bd1dd86febfc77d350c87f309b9ec56747db27f
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2021-03-17 02:58:16 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chart_app_by_sda/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
