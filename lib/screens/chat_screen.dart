8e2bfbb10bd3bf62b941130b567e66e7019bff59
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2021-03-24 02:02:45 +0500

    318. Getting Started with Flutter & the Firebase SDK

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
@@ -11,6 +12,19 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('This works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/chSh9GT9SXd2GkCZ8Hq4/messages')
+              .snapshots()
+              .listen((data) {
+                data.documents.forEach((element) {
+                  print(element['text']);
+                });
+          });
+        },
       ),
     );
   }
